c891a143fdbe990b7e91b8ba7046e60824aa6da3
klaajane <karame.laajane5@gmail.com>
2025-09-01 20:37:21 -0500

    Time: 208 ms (72.53%), Space: 0B (100%) - LeetHub

---
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -0,0 +1,12 @@
+--
+SELECT
+     score
+    ,DENSE_RANK() OVER (ORDER BY score DESC) as rank
+FROM
+     Scores
+ORDER BY 1 DESC
+
+
+-- query: rank of the scores (DESC)
+-- tie = same ranking & next ranking number after a tie (DENSE_RANK())
+-- order by score in DESC
