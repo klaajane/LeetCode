d71bc0d052780eb68248e0535411075d8832834c
klaajane <karame.laajane5@gmail.com>
2025-09-02 15:54:01 -0500

    Update 0197-rising-temperature.sql

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,13 +1,18 @@
 --------------------------------------------- SOLUTION ------------------------------------------
 SELECT
      w1.id AS "Id"
+    ,w1.recordDate AS "Current Date" -- Enhancement
+    ,w2.recordDate AS "Previous Date" -- Enhancement
+    ,w1.temperature AS "Current Temperature" -- Enhancement
+    ,w2.temperature AS "Prev Temperature" -- Enhancement
+    ,w1.temperature - w2.temperature AS "Temperature Change (#)" -- Enhancement
 FROM
     Weather w1
 INNER JOIN 
     Weather w2
     ON
-    w1.recordDate- w2.recordDate = 1
-    AND
+    w1.recordDate = w2.recordDate INTERVAL '1 day'
+WHERE
     w1.temperature > w2.temperature
 ---------------------------------------------- NOTES --------------------------------------------
 --> find all dates' id with higher temp compared to yesterday
