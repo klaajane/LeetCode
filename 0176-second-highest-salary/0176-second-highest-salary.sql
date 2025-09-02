a20c7d31e554d41903b2cb6b7ba63f74da70367a
klaajane <karame.laajane5@gmail.com>
2025-09-01 20:02:29 -0500

    Update 0176-second-highest-salary.sql

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,6 +1,14 @@
+--------------------------------------------- SOLUTION -------------------------------------------
 SELECT 
     IFNULL(MAX(salary), NULL) AS SecondHighestSalary 
 FROM 
     (SELECT DISTINCT salary FROM Employee) AS distinct_salaries
 WHERE 
     salary < (SELECT MAX(salary) FROM Employee)
+---------------------------------------------- NOTES --------------------------------------------
+--> 2nd highest DISTINCT salary
+--> return NULL when no 2nd highest salary exists
+------------------------------------------- OPTIMIZATION ----------------------------------------
+--> DISTINCT & Subquery: limit unecessary checks, in case the data contains duplicates
+--> IFNULL: Handles edge cases if the data only contains one salary
+-------------------------------------------------------------------------------------------------
