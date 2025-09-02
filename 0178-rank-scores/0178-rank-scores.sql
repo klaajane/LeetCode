20affb38f76e7af91f7dbb79593a0a908e74aea7
klaajane <karame.laajane5@gmail.com>
2025-09-01 20:39:00 -0500

    Time: 221 ms (51.18%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,12 +1,12 @@
---
+--------------------------------------------- SOLUTION -------------------------------------------
 SELECT
      score
     ,DENSE_RANK() OVER (ORDER BY score DESC) as rank
 FROM
      Scores
 ORDER BY 1 DESC
-
-
+---------------------------------------------- NOTES --------------------------------------------
 -- query: rank of the scores (DESC)
 -- tie = same ranking & next ranking number after a tie (DENSE_RANK())
 -- order by score in DESC
+-------------------------------------------------------------------------------------------------
