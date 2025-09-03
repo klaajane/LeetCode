0570cb010cc24dd13e34b451183d050e19a26da9
klaajane <karame.laajane5@gmail.com>
2025-09-03 09:59:17 -0500

    Update 0262-trips-and-users.sql

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -1,42 +1,44 @@
 --------------------------------------------- SOLUTION ------------------------------------------
-WITH banned_users AS (
-    SELECT users_id, role
-    FROM Users
-    WHERE banned = 'Yes'
-)
-
 SELECT
-    t.request_at AS "Day",
-    ROUND(
+     request_at AS "Day"
+    ,ROUND( -- 1.00 * can be used to force floating-point (decimal) devision instead of integer
         SUM(CASE 
-                WHEN t.status IN ('cancelled_by_driver', 'cancelled_by_client') 
+                WHEN status IN ('cancelled_by_driver', 'cancelled_by_client') 
                 THEN 1 
                 ELSE 0 
             END)::numeric / COUNT(*),
         2
     ) AS "Cancellation Rate"
+    ,SUM(CASE 
+                WHEN status IN ('cancelled_by_driver', 'cancelled_by_client') 
+                THEN 1 
+                ELSE 0
+        END)
+    AS "# of Cancelled Trips" -- Enhancement
+    ,COUNT(*) AS "Total Trips" -- Enhancement
 FROM
-    Trips t
+    Trips
 WHERE
-    NOT EXISTS (
-        SELECT 1 
-        FROM banned_users bu 
-        WHERE bu.users_id = t.driver_id AND bu.role = 'driver'
+    driver_id NOT IN (
+        SELECT users_id 
+        FROM Users 
+        WHERE banned = 'Yes' AND role = 'driver'
     )
-    AND NOT EXISTS (
-        SELECT 1 
-        FROM banned_users bu 
-        WHERE bu.users_id = t.client_id AND bu.role = 'client'
+    AND client_id NOT IN (
+        SELECT users_id 
+        FROM Users 
+        WHERE banned = 'Yes' AND role = 'client'
     )
-    AND t.request_at BETWEEN '2013-10-01' AND '2013-10-03'
+    AND
+    request_at BETWEEN '2013-10-01' and '2013-10-03'
 GROUP BY
-    t.request_at
-ORDER BY
-    t.request_at
+    request_at
 ---------------------------------------------- NOTES --------------------------------------------
---> cancellation rate = # canceled (client or driver) request from UNBANNED users / # unbanned users request
+--> cancellation rate = # canceled (client or driver) request from UNBANNED users 
+--                      / # unbanned users request
 --> find cancellation rate between "2013-10-01" and "2013-10-03" with at least one trip
 --> Round cancelllation rate to 2 decimal points
------------------------------------ OPTIMIZATION & ENHANCEMENT -----------------------------------
---> 
+----------------------------------- OPTIMIZATION & ENHANCEMENT ----------------------------------
+--> stakeholders may want to see the # of cancelled trips and total trips. This is also beneficial
+-- to data validation
 -------------------------------------------------------------------------------------------------
