4a04429155bef00630855e487b3a5aa613a7f2c8
klaajane <karame.laajane5@gmail.com>
2025-09-03 11:03:43 -0500

    Time: 596 ms (28.86%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,29 +1,29 @@
 --------------------------------------------- SOLUTION ------------------------------------------
-WITH activity_log AS (
-    SELECT 
-        player_id
-        ,event_date
-        ,LEAD(event_date) OVER (PARTITION BY player_id ORDER BY event_date ASC) AS next_event_date
-        ,ROW_NUMBER() OVER (PARTITION BY player_id ORDER BY event_date ASC) AS r
-    FROM
-        Activity)
+WITH first_log AS (
+    SELECT
+         player_id
+        ,MIN(event_date) AS first_login_date
+    FROM 
+        Activity
+    GROUP BY
+        player_id)
 
 SELECT
     ROUND(
-        1.00 * SUM(CASE
-                        WHEN next_event_date = event_date + INTERVAL '1 day'
-                        THEN 1
-                        ELSE 0
-                   END) / COUNT(DISTINCT player_id),
-     2
-    ) AS "fraction"
-FROM
-    activity_log
-WHERE
-    r = 1
+        1.0 * SUM(CASE
+                    WHEN a.player_id IS NOT NULL
+                    THEN 1
+                    ELSE 0
+                  END) / NULLIF(COUNT(1), 0),
+     2) AS "fraction"
+FROM 
+    first_log ft
+LEFT JOIN
+    activity a
+    ON ft.player_id = a.player_id
+    AND
+    a.event_date = first_login_date + INTERVAL '1 day'
 ---------------------------------------------- NOTES --------------------------------------------
 --> fraction of players who logged in again after the day they first logged in
 --> round to 2 decimal places
------------------------------------ OPTIMIZATION & ENHANCEMENT ----------------------------------
---> 
 -------------------------------------------------------------------------------------------------
