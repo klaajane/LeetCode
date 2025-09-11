c549b8134be00e240c3e9730d5eb96c2cb19c8bc
klaajane <karame.laajane5@gmail.com>
2025-09-10 19:50:57 -0500

    Time: 176 ms (92.55%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,18 @@
+--------------------------------------------- SOLUTION ------------------------------------------
+SELECT
+    id
+    ,COALESCE(
+        CASE 
+            WHEN id % 2 = 0 THEN LAG(student) OVER (ORDER BY id)
+            ELSE LEAD(student) OVER (ORDER BY id)
+     END, 
+     student) 
+     AS "student"
+FROM
+    Seat
+---------------------------------------------- NOTES --------------------------------------------
+--> swap seat id of every two consecutive students
+--> number of last student is odd => don't swap
+------------------------------------------- OPTIMIZATION ----------------------------------------
+--> 
+-------------------------------------------------------------------------------------------------
