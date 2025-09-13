d2393f8c464a1e81aeab314d388c80d9863bb643
klaajane <karame.laajane5@gmail.com>
2025-09-13 16:26:14 -0500

    Update 1174-immediate-food-delivery-ii.sql

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,6 +1,6 @@
 -------------------------------------------- SOLUTION -------------------------------------------
 SELECT
-    ROUND( SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END)::numeric
+    ROUND( SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END)::NUMERIC
             * 100.0 / COUNT(*)
             ,2) AS immediate_percentage
 FROM
