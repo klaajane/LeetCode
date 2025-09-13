c765830691dd8d227b155436fb74c8ee83b3cd23
klaajane <karame.laajane5@gmail.com>
2025-09-13 16:26:00 -0500

    Update 1174-immediate-food-delivery-ii.sql

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,3 +1,16 @@
-SELECT (ROUND (sum(IF (order_date=customer_pref_delivery_date, 1, 0)) / count(*) , 4) * 100) as immediate_percentage
-FROM Delivery
-WHERE (customer_id,order_date) in (select customer_id,min(order_date) from delivery group by customer_id)
+-------------------------------------------- SOLUTION -------------------------------------------
+SELECT
+    ROUND( SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END)::numeric
+            * 100.0 / COUNT(*)
+            ,2) AS immediate_percentage
+FROM
+    Delivery
+WHERE
+    order_date IN (SELECT MIN(order_date) FROM Delivery GROUP BY customer_id)
+---------------------------------------------- NOTES --------------------------------------------
+--> immediate: customer's preferred delivery date = order date (otherwise scheduled)
+--> first order: earliest order date
+--> customers have unique orders
+--> find % of immmediate order in the first orders of all customers
+--> round to 2 decimal places
+-------------------------------------------------------------------------------------------------
