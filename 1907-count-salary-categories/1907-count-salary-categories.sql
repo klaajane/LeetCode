2752f2fed8aaea135aa7c10b935dc91462d6e11e
klaajane <karame.laajane5@gmail.com>
2025-09-23 18:21:00 -0500

    Time: 3183 ms (97.97%), Space: 0B (100%) - LeetHub

---
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -0,0 +1,29 @@
+-------------------------------------------- SOLUTION -------------------------------------------
+WITH salary_category AS (SELECT 
+                            'Low Salary' AS category,
+                            SUM(CASE WHEN income < 20000 THEN 1 ELSE 0 END) AS "accounts_count"
+                        FROM
+                            accounts
+                            UNION
+                        SELECT 
+                            'Average Salary' AS category,
+                            SUM(CASE WHEN income >= 20000 AND income <= 50000 THEN 1 ELSE 0 END) AS "accounts_count"
+                        FROM
+                            accounts
+                            UNION
+                        SELECT 
+                            'High Salary' AS category,
+                            SUM(CASE WHEN income > 50000 THEN 1 ELSE 0 END) AS "accounts_count"
+                        FROM
+                            accounts)
+SELECT 
+    *
+FROM
+    salary_category
+ORDER BY
+    accounts_count DESC
+---------------------------------------------- NOTES --------------------------------------------
+--> "Low Salary": All the salaries strictly less than $20000.
+--> "Average Salary": All the salaries in the inclusive range [$20000, $50000].
+--> "High Salary": All the salaries strictly greater than $50000.
+-------------------------------------------------------------------------------------------------
