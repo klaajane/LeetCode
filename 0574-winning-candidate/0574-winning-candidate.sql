74f43f4d45aea2a3cf90dfe6082d1c1165d28465
klaajane <karame.laajane5@gmail.com>
2025-10-08 18:11:59 -0500

    Time: 181 ms (85.53%), Space: 0B (100%) - LeetHub

--- 0574-winning-candidate/0574-winning-candidate.sql
+++ 0574-winning-candidate/0574-winning-candidate.sql
@@ -1,18 +1,14 @@
 -------------------------------------------- SOLUTION -------------------------------------------
 SELECT 
-    name
+    c.name
 FROM
-    Candidate
-WHERE
-    id IN (SELECT 
-            candidateId 
-           FROM 
-            Vote 
-           GROUP BY 
-            candidateId 
-           ORDER BY 
-            COUNT(id) 
-           DESC LIMIT 1)
+    Candidate c
+LEFT JOIN
+    Vote v ON v.candidateId = c.id
+GROUP BY 1
+ORDER BY 
+    COUNT(*) DESC
+LIMIT 1
 ---------------------------------------------- NOTES --------------------------------------------
 --> the name of the winning candidate (largest votes)
 -------------------------------------------------------------------------------------------------
