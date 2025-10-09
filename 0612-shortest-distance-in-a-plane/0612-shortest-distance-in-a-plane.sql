fa6fcf1167c6372a24be968bd2b582b6210aa7dd
klaajane <karame.laajane5@gmail.com>
2025-10-08 23:01:43 -0500

    Time: 227 ms (98.86%), Space: 0B (100%) - LeetHub

---
+++ 0612-shortest-distance-in-a-plane/0612-shortest-distance-in-a-plane.sql
@@ -0,0 +1,18 @@
+-------------------------------------------- SOLUTION -------------------------------------------
+SELECT
+    ROUND(
+         SQRT(
+            MIN(POWER(p2.x - p1.x,2) + POWER(p2.y - p1.y, 2))
+             )::numeric,
+         2
+        ) AS shortest 
+FROM
+    Point2D p1
+CROSS JOIN
+    Point2D p2 
+WHERE 
+    (p1.x, p1.y) <> (p2.x, p2.y)
+---------------------------------------------- NOTES --------------------------------------------
+--> report the SHORTEST distance between two points
+--> ROUND to 2 decimal points
+-------------------------------------------------------------------------------------------------
