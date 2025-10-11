5e4ae8aabc7fdd28ef62605cf6ad15392e8a975f
klaajane <karame.laajane5@gmail.com>
2025-10-11 14:51:38 -0500

    Time: 336 ms (95.07%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,20 +1,20 @@
 -------------------------------------------- SOLUTION -------------------------------------------
-WITH qualified_products1 AS (SELECT
+WITH qualified_products AS (SELECT
                                 *,
                                 ROW_NUMBER() OVER (PARTITION BY product_id
                                                     ORDER BY change_date DESC) AS rnk 
                             FROM
                                 Products
                             WHERE
-                                change_date <= '2019-08-16'),
-
-qualified_products2 AS (SELECT product_id, 10 AS "price"
-                        FROM products
-                        WHERE product_id NOT IN (SELECT product_id FROM qualified_products1))
-
-SELECT product_id, new_price AS "price" FROM qualified_products1 WHERE rnk = 1
-    UNION
-SELECT product_id, price FROM qualified_products2
+                                change_date <= '2019-08-16')
+SELECT
+    DISTINCT p.product_id,
+    COALESCE(q.new_price, 10) AS price
+FROM
+    products p
+LEFT JOIN
+    qualified_products q ON q.product_id = p.product_id
+                        AND rnk = 1
 ---------------------------------------------- NOTES --------------------------------------------
 --> all products have a price of 10
 --> find prices on the date 2019-08-16
