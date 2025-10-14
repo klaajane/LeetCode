b2bf67246936581a54e53046b422ad1620c59ee1
klaajane <karame.laajane5@gmail.com>
2025-10-14 09:02:04 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1445-apples-oranges/1445-apples-oranges.sql
+++ 1445-apples-oranges/1445-apples-oranges.sql
@@ -1,16 +1,10 @@
 --------------------------------------------- SOLUTION ------------------------------------------
-SELECT
-    sale_date,
-    SUM(CASE WHEN fruit = 'apples' THEN sold_num ELSE 0 END)
-    -
-    SUM(CASE WHEN fruit = 'oranges' THEN sold_num ELSE 0 END)
-    AS "diff"
-FROM
-    Sales
-GROUP BY
-    sale_date
-ORDER BY
-    sale_date
+SELECT sale_date, 
+    SUM((SELECT sold_num WHERE fruit = 'apples')) - 
+    SUM((SELECT sold_num WHERE fruit = 'oranges')) AS diff
+FROM Sales
+GROUP BY sale_date
+ORDER BY sales_date
 ---------------------------------------------- NOTES --------------------------------------------
 --> report the difference between the # of apples and oranges sold each day
 --> order by sale_date
