f3f6d5d23bd7c776622e8b4d732349783c1ede47
klaajane <karame.laajane5@gmail.com>
2025-10-14 09:02:04 -0500

    Time: 173 ms (87.86%), Space: 0B (100%) - LeetHub

--- 1445-apples-oranges/1445-apples-oranges.sql
+++ 1445-apples-oranges/1445-apples-oranges.sql
@@ -4,7 +4,7 @@ SELECT sale_date,
     SUM((SELECT sold_num WHERE fruit = 'oranges')) AS diff
 FROM Sales
 GROUP BY sale_date
-ORDER BY sales_date
+ORDER BY sale_date
 ---------------------------------------------- NOTES --------------------------------------------
 --> report the difference between the # of apples and oranges sold each day
 --> order by sale_date
