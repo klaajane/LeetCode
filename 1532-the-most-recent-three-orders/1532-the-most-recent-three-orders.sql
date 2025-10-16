d3ed4570bea6c0a89034c0009b3c9bbc75216596
klaajane <karame.laajane5@gmail.com>
2025-10-16 18:39:57 -0500

    Time: 395 ms (29%), Space: 0B (100%) - LeetHub

--- 1532-the-most-recent-three-orders/1532-the-most-recent-three-orders.sql
+++ 1532-the-most-recent-three-orders/1532-the-most-recent-three-orders.sql
@@ -1,22 +1,5 @@
 --------------------------------------------- SOLUTION ------------------------------------------
-WITH customers_less_than_3_orders AS (
-    SELECT
-        c.name,
-        o.customer_id,
-        o.order_id,
-        o.order_date
-    FROM
-        orders o
-    INNER JOIN
-        customers c
-        ON c.customer_id = o.customer_id
-    WHERE o.customer_id IN  (SELECT
-                                customer_id
-                            FROM orders
-                            GROUP BY 1
-                            HAVING COUNT(*) < 3)),
-
-customers_more_than_3_orders AS (
+WITH orders_ranked AS (
     SELECT
         c.name,
         o.customer_id,
@@ -28,15 +11,16 @@ customers_more_than_3_orders AS (
         orders o
     INNER JOIN
         customers c
-        ON c.customer_id = o.customer_id
-    WHERE
-        o.customer_id NOT IN (SELECT customer_id 
-                              FROM customers_less_than_3_orders))
+        ON c.customer_id = o.customer_id)
 
-SELECT name "customer_name", customer_id, order_id, order_date FROM customers_less_than_3_orders
-    UNION ALL
-SELECT name "customer_name", customer_id, order_id, order_date FROM customers_more_than_3_orders
-    WHERE rnk <= 3    
+SELECT 
+    name "customer_name", 
+    customer_id, 
+    order_id, 
+    order_date 
+FROM 
+    orders_ranked
+WHERE rnk <= 3    
 ORDER BY customer_name ASC, customer_id ASC, order_date DESC
 ---------------------------------------------- NOTES --------------------------------------------
 --> return the most recent 3 order for each users (return ALL if total order is LESS than 3)
