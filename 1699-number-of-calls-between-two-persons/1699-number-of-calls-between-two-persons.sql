3a033efc7ccf2ff35d215244e73ee708e5cf4b53
klaajane <karame.laajane5@gmail.com>
2025-10-18 14:08:00 -0500

    Time: 398 ms (69.63%), Space: 0B (100%) - LeetHub

---
+++ 1699-number-of-calls-between-two-persons/1699-number-of-calls-between-two-persons.sql
@@ -0,0 +1,26 @@
+--------------------------------------------- SOLUTION ------------------------------------------
+WITH calls_records AS (
+    SELECT
+        CASE
+            WHEN from_id > to_id THEN to_id
+            ELSE from_id
+        END AS "person1",
+        CASE
+            WHEN from_id < to_id THEN to_id
+            ELSE from_id
+        END AS "person2",
+        duration
+    FROM
+        calls)
+
+SELECT
+    person1,
+    person2,
+    COUNT(*) "call_count",
+    SUM(duration) "total_duration"
+FROM
+    calls_records
+GROUP BY 1, 2
+---------------------------------------------- NOTES --------------------------------------------
+--> report # of calls and call duration between each pair of disinct people
+-------------------------------------------------------------------------------------------------
