4f77529c44a8a4684be074116d07b0bf1c285f73
klaajane <karame.laajane5@gmail.com>
2025-10-20 20:58:53 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1949-strong-friendship/1949-strong-friendship.sql
@@ -0,0 +1,29 @@
+-------------------------------------------- SOLUTION -------------------------------------------
+WITH all_friends AS
+    (SELECT user1_id, user2_id FROM friendship
+        UNION ALL
+    SELECT user2_id, user1_id FROM friendship)
+
+SELECT
+    a1.user1_id,
+    a2.user1_id "user2_id",
+    COUNT(DISTINCT a1.user2_id) AS common_friend
+FROM
+    all_friends a1
+    JOIN
+        all_friends a2
+        -- retrieve only rows with common friends
+        ON a1.user2_id = a2.user2_id
+        -- avoid duplicates
+        AND a1.user1_id < a2.user1_id
+-- users have to be friends        
+WHERE
+    (a1.user1_id, a2.user2_id) IN (SELECT * FROM all_friends)
+    AND
+    (a2.user1_id, a1.user2_id) IN (SELECT * FROM all_friends)
+GROUP BY 1, 2
+HAVING COUNT(DISTINCT a1.user2_id) >= 3
+---------------------------------------------- NOTES --------------------------------------------
+--> strong friendships: x and y have at least 3 common friends
+--> user1_id < user2_id to avoid duplicates
+-------------------------------------------------------------------------------------------------
