8c547aa41ca8532080327d78c4e990f05af001c5
klaajane <karame.laajane5@gmail.com>
2025-10-20 21:18:16 -0500

    Time: 552 ms (37.5%), Space: 0B (100%) - LeetHub

--- 1951-all-the-pairs-with-the-maximum-number-of-common-followers/1951-all-the-pairs-with-the-maximum-number-of-common-followers.sql
+++ 1951-all-the-pairs-with-the-maximum-number-of-common-followers/1951-all-the-pairs-with-the-maximum-number-of-common-followers.sql
@@ -1,16 +1,24 @@
 -------------------------------------------- SOLUTION -------------------------------------------
+WITH follower_count AS (
+    SELECT
+        r1.user_id "user1_id",
+        r2.user_id "user2_id",
+        COUNT(DISTINCT r1.follower_id) AS "followers_count"
+    FROM
+        relations r1
+        INNER JOIN 
+            relations r2
+            ON r1.follower_id = r2.follower_id
+            AND r1.user_id < r2.user_id
+    GROUP BY 1, 2)
+
 SELECT
-    r1.user_id "user1_id",
-    r2.user_id "user2_id"
+    user1_id,
+    user2_id
 FROM
-    relations r1
-    INNER JOIN 
-        relations r2
-        ON r1.follower_id = r2.follower_id
-        AND r1.user_id < r2.user_id
-GROUP BY 1, 2
-HAVING COUNT(DISTINCT r1.follower_id) >= 3
+    follower_count
+WHERE
+    followers_count = (SELECT MAX(followers_count) FROM follower_count)
 ---------------------------------------------- NOTES --------------------------------------------
---> strong friendships: x and y have at least 3 common friends
---> user1_id < user2_id to avoid duplicates
+--> find all the pairs of users with maximum # of common followers
 -------------------------------------------------------------------------------------------------
