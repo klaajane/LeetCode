9ce8551b172475d9851c9e5f5c4a4858b4fa1993
klaajane <karame.laajane5@gmail.com>
2025-10-22 17:17:30 -0500

    Time: 213 ms (94.87%), Space: 0B (100%) - LeetHub

--- 2084-drop-type-1-orders-for-customers-with-type-0-orders/2084-drop-type-1-orders-for-customers-with-type-0-orders.sql
+++ 2084-drop-type-1-orders-for-customers-with-type-0-orders/2084-drop-type-1-orders-for-customers-with-type-0-orders.sql
@@ -10,15 +10,25 @@ WITH order_type AS (
             END) AS "ordertype0_count"
     FROM
         Orders
-    GROUP BY 1)
+    GROUP BY 1
+),
 
-SELECT order_id, customer_id, order_type 
-FROM orders 
-WHERE order_type = 0 AND customer_id IN (SELECT customer_id FROM order_type WHERE ordertype0_count >= 1)
-    UNION ALL
+orderstype0 AS (
+SELECT o.order_id, o.customer_id, o.order_type 
+FROM orders o
+WHERE o.order_type = 0 AND EXISTS (SELECT 1 FROM order_type ot WHERE 
+o.customer_id = ot.customer_id AND ordertype0_count >= 1)
+),
+
+orderstype_no_0 AS(
 SELECT o.order_id, o.customer_id, o.order_type 
 FROM orders o 
 WHERE EXISTS (SELECT 1 FROM order_type ot WHERE ot.customer_id = o.customer_id AND ordertype0_count = 0)
+)
+
+SELECT * FROM orderstype0
+    UNION ALL
+SELECT * FROM orderstype_no_0
 ---------------------------------------------- NOTES --------------------------------------------
 --> report all the orders:
     --> customer has at least order of type 0, no type 1
