58ca6cb2d34ac087a50d864b14a9bfc784e1fb66
klaajane <karame.laajane5@gmail.com>
2025-10-23 19:19:51 -0500

    Time: 223 ms (50%), Space: 0B (100%) - LeetHub

--- 2228-users-with-two-purchases-within-seven-days/2228-users-with-two-purchases-within-seven-days.sql
+++ 2228-users-with-two-purchases-within-seven-days/2228-users-with-two-purchases-within-seven-days.sql
@@ -1,29 +1,15 @@
 --------------------------------------------- SOLUTION ------------------------------------------
-WITH next_purchases AS (  
-    SELECT
-        purchase_id,
-        user_id,
-        purchase_date,
-        LEAD(purchase_date) OVER (PARTITION BY user_id
-                                    ORDER BY purchase_date ASC) AS "next_purchases_date"
-    FROM
-        purchases p1
-    WHERE EXISTS (SELECT 1 FROM purchases p GROUP BY p.user_id HAVING COUNT(*) >= 2 AND p.user_id = p1.user_id)
-),
-
-difference_in_dates AS (
-    SELECT
-        purchase_id,
-        user_id,
-        next_purchases_date - purchase_date "days_diff"
-    FROM
-        next_purchases
-    WHERE
-        next_purchases_date IS NOT NULL
-)
-
-SELECT DISTINCT user_id FROM difference_in_dates
-WHERE days_diff <= 7
+SELECT DISTINCT
+    p1.user_id
+FROM 
+    purchases p1
+    JOIN  
+        purchases p2
+        ON p1.user_id = p2.user_id
+        AND p1.purchase_id <> p2.purchase_id
+        AND p2.purchase_date >= p1.purchase_date
+WHERE
+    ABS(p1.purchase_date - p2.purchase_date) <= 7
 ---------------------------------------------- NOTES --------------------------------------------
 --> report IDs of the users with more than two purchaes <= 7
 --> order by user_id
