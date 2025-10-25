5c6fe7d7013fbbae9a50a27889ceda3cc72188f6
klaajane <karame.laajane5@gmail.com>
2025-10-25 13:27:49 -0500

    Time: 218 ms (59.52%), Space: 0B (100%) - LeetHub

--- 2298-tasks-count-in-the-weekend/2298-tasks-count-in-the-weekend.sql
+++ 2298-tasks-count-in-the-weekend/2298-tasks-count-in-the-weekend.sql
@@ -2,14 +2,17 @@
 WITH day_of_week AS (
     SELECT
         task_id,
-        EXTRACT(DOW FROM submit_date) AS "day_week_number"
+        CASE
+            WHEN EXTRACT(DOW FROM submit_date) BETWEEN 1 AND 5 THEN 'working'
+            ELSE 'weekend'
+        END AS "day_type"
     FROM
         tasks
 )
 
 SELECT
-    SUM(CASE WHEN day_week_number = 0 OR day_week_number = 6 THEN 1 ELSE 0 END) AS "weekend_cnt",
-    SUM(CASE WHEN day_week_number BETWEEN 1 AND 5 THEN 1 ELSE 0 END) AS "working_cnt"
+    SUM(CASE WHEN day_type = 'weekend' THEN 1 ELSE 0 END) AS "weekend_cnt",
+    SUM(CASE WHEN day_type = 'working' THEN 1 ELSE 0 END) AS "working_cnt"
 FROM
     day_of_week
 ---------------------------------------------- NOTES --------------------------------------------
