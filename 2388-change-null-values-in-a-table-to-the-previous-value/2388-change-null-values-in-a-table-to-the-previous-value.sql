81550dbd002d751792bf7614fc72a22ead4e5f4b
klaajane <karame.laajane5@gmail.com>
2025-10-26 15:19:22 -0500

    Time: 250 ms (33.33%), Space: 0B (100%) - LeetHub

--- 2388-change-null-values-in-a-table-to-the-previous-value/2388-change-null-values-in-a-table-to-the-previous-value.sql
+++ 2388-change-null-values-in-a-table-to-the-previous-value/2388-change-null-values-in-a-table-to-the-previous-value.sql
@@ -3,18 +3,27 @@ WITH drinks AS (
     SELECT
         id,
         drink,
-        COALESCE(LAG(drink) OVER (), drink) AS "prev_drink",
-        ---ROW_NUMBER() OVER () AS "rnk",
-        FIRST_VALUE(drink) OVER () AS "first_value"
+        ROW_NUMBER() OVER () AS "r" 
     FROM
         CoffeeShop
-)
+),
     
-SELECT 
+drinks2 AS (
+    SELECT 
+        id,
+        drink,
+        r,
+        SUM(CASE WHEN drink IS NULL THEN 0 ELSE 1 END) OVER (order by r) AS "grp_id"
+    FROM
+        drinks
+)
+
+SELECT
     id,
-    COALESCE(drink, prev_drink, first_value) AS "drink"
+    first_value(drink) OVER (PARTITION BY grp_id
+                             ORDER BY r) AS "drink"
 FROM
-    drinks
+    drinks2
 ---------------------------------------------- NOTES --------------------------------------------
 --> replace the null values of the drink with the non-NULL name of the drink from prev row 
 -------------------------------------------------------------------------------------------------
