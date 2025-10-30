1746ebac131e2eb283dc34e6cd94e522be470577
klaajane <karame.laajane5@gmail.com>
2025-10-30 18:31:19 -0500

    Time: 420 ms (61.11%), Space: 0B (100%) - LeetHub

--- 2701-consecutive-transactions-with-increasing-amounts/2701-consecutive-transactions-with-increasing-amounts.sql
+++ 2701-consecutive-transactions-with-increasing-amounts/2701-consecutive-transactions-with-increasing-amounts.sql
@@ -1,31 +1,50 @@
-# Write your MySQL query statement below
-
-WITH CTE1 AS(
-    SELECT 
-        *
-        ,LAG(amount) OVER(PARTITION BY customer_id ORDER BY transaction_date) AS prev_amount
-        ,LAG(transaction_date) OVER(PARTITION BY customer_id ORDER BY transaction_date) AS pre_transaction_date
-    FROM 
+-------------------------------------------- SOLUTION -------------------------------------------
+-- previous date, amount for each date,amount
+WITH transactions_journal AS (
+    SELECT
+        transaction_id,
+        customer_id,
+        transaction_date,
+        LAG(transaction_date) OVER (PARTITION BY customer_id
+                                    ORDER BY transaction_date) AS "prev_date",
+        amount,
+        LAG(amount) OVER (PARTITION BY customer_id
+                            ORDER BY transaction_date) AS "prev_amount"
+    FROM
         Transactions
     ORDER BY
         customer_id, transaction_date
 ),
 
-CTE2 AS(
-    SELECT 
-        *
-        ,TO_DAYS(transaction_date) - RANK() OVER(PARTITION BY customer_id ORDER BY transaction_date) AS GRP
-    FROM CTE1 
-    WHERE amount > prev_amount AND DATEDIFF(transaction_date, pre_transaction_date) = 1
+consecutive_date_amount AS (
+    SELECT
+        transaction_id,
+        customer_id,
+        prev_date,
+        transaction_date,
+        prev_amount,
+        amount,
+        (transaction_date - DATE '0001-01-01')
+        - 
+        RANK() OVER (PARTITION BY customer_id ORDER BY transaction_date) AS "grp_id"
+    FROM transactions_journal
+    WHERE transaction_date = prev_date + INTERVAL '1 Day' 
+    AND amount > prev_amount 
 )
 
-
-SELECT 
+SELECT
     customer_id,
-    MIN(pre_transaction_date) AS consecutive_start,
+    MIN(prev_date) AS consecutive_start,
     MAX(transaction_date) AS consecutive_end
-FROM 
-    CTE2
-GROUP BY 
-    customer_id, GRP
-HAVING COUNT(GRP) >= 2
+FROM
+    consecutive_date_amount
+GROUP BY
+    customer_id, grp_id
+HAVING
+    COUNT(grp_id) >= 2
+---------------------------------------------- NOTES --------------------------------------------
+--> gap and island problem
+--> report customers who have made consecutive transactions with increasing amount for at least
+    -- 3 days 
+--> report customer_id, consecutive_start and _end (Order ASC)
+-------------------------------------------------------------------------------------------------
