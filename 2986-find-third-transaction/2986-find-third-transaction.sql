c2358050c0650823027591bec0b701d3a44196ba
klaajane <karame.laajane5@gmail.com>
2025-11-02 11:10:08 -0600

    Time: 538 ms (25.93%), Space: 0B (100%) - LeetHub

--- 2986-find-third-transaction/2986-find-third-transaction.sql
+++ 2986-find-third-transaction/2986-find-third-transaction.sql
@@ -1,40 +1,26 @@
 -------------------------------------------- SOLUTION -------------------------------------------
-WITH transaction_series AS (
+WITH transactions_history AS (
     SELECT
         user_id,
         spend,
         transaction_date,
-        ROW_NUMBER() OVER (PARTITION BY user_id
-                            ORDER BY transaction_date) AS "r_id"
+        LAG(spend, 1) OVER (PARTITION BY user_id ORDER BY transaction_date) AS "first_previous_date",
+        LAG(spend, 2) OVER (PARTITION BY user_id ORDER BY transaction_date) AS "second_previous_date",
+        DENSE_RANK() OVER (PARTITION BY user_id ORDER BY transaction_date) AS "rnk"
     FROM
-        Transactions
-),
-
-qualified_users AS (
-    SELECT user_id FROM transaction_series
-    WHERE r_id <= 3
-    GROUP BY user_id
-    HAVING COUNT(*) <= 3
+        transactions
 )
 
-SELECT DISTINCT
-    t1.user_id,
-    t1.spend AS "third_transaction_spend",
-    t1.transaction_date AS "third_transaction_date"
+SELECT
+    user_id,
+    spend AS "third_transaction_spend",
+    transaction_date AS "third_transaction_date"
 FROM
-    transaction_series t1
-        INNER JOIN
-            transaction_series t2
-                ON t1.user_id = t2.user_id AND t1.spend > t2.spend
-        INNER JOIN
-            transaction_series t3
-                ON t1.user_id = t3.user_id AND t1.spend > t3.spend
+    transactions_history
 WHERE
-    t1.r_id = 3
-    AND 
-    t1.user_id IN (SELECT user_id FROM qualified_users)
-ORDER BY 
-    user_id
+    rnk = 3 
+    AND spend > first_previous_date 
+    AND spend > second_previous_date
 ---------------------------------------------- NOTES --------------------------------------------
 --> find 3rd transaction (if they have at least 3 transactions) where spending 3rd > 2nd & 1st
 --> order by user_id ASC
