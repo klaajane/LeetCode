7e80af369654a4aa1ecddfeee1855b59b0f1b881
klaajane <karame.laajane5@gmail.com>
2026-03-07 12:01:11 -0600

    Time: 217 ms (24%), Space: 0B (100%) - LeetHub

--- 3384-team-dominance-by-pass-success/3384-team-dominance-by-pass-success.sql
+++ 3384-team-dominance-by-pass-success/3384-team-dominance-by-pass-success.sql
@@ -1,25 +1,24 @@
-WITH PassesWithHalf AS (
-    SELECT 
-        p.pass_from, 
-        p.pass_to, 
-        t1.team_name AS from_team,
-        t2.team_name AS to_team,
-        CASE 
+-- Write your PostgreSQL query statement below
+WITH teams_points AS (
+    SELECT
+        t1.team_name as "team_name",
+        CASE
             WHEN p.time_stamp <= '45:00' THEN 1
             ELSE 2
-        END AS half_number,
-        CASE 
+        END AS "half_number",
+        CASE
             WHEN t1.team_name = t2.team_name THEN 1
             ELSE -1
-        END AS dominance
-    FROM Passes p
-    JOIN Teams t1 ON p.pass_from = t1.player_id
-    JOIN Teams t2 ON p.pass_to = t2.player_id
-)
-SELECT 
-    from_team AS team_name, 
-    half_number, 
-    SUM(dominance) AS dominance
-FROM PassesWithHalf
-GROUP BY from_team, half_number
-ORDER BY from_team, half_number;
+        END AS "points"
+    FROM passes p
+    JOIN teams t1 ON p.pass_from = t1.player_id
+    JOIN teams t2 ON p.pass_to = t2.player_id)
+
+--- NO NEED TO UNION THE DATA 
+SELECT
+    team_name,
+    half_number,
+    SUM(points) AS "dominance"
+FROM teams_points
+GROUP BY team_name, half_number
+ORDER BY team_name, half_number
