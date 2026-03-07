0371ce475a93a9f488233229a326048450ec978c
klaajane <karame.laajane5@gmail.com>
2026-03-06 23:02:44 -0600

    Time: 182 ms (85.29%), Space: 0B (100%) - LeetHub

---
+++ 3368-first-letter-capitalization/3368-first-letter-capitalization.sql
@@ -0,0 +1,11 @@
+-- Write your PostgreSQL query statement below
+
+
+-- I'm thinking of using split function to split words, capitalize letter
+-- then group them back together, let's see it this works: 
+
+SELECT
+    content_id,
+    content_text AS "original_text",
+    INITCAP(content_text) AS "converted_text"
+FROM user_content
