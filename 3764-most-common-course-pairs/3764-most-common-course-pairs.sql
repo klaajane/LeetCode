48d734a9b394030aaf33c20490485aa66fbb1ddf
klaajane <karame.laajane5@gmail.com>
2026-03-07 23:44:46 -0600

    Time: 184 ms (99.15%), Space: 0B (100%) - LeetHub

--- 3764-most-common-course-pairs/3764-most-common-course-pairs.sql
+++ 3764-most-common-course-pairs/3764-most-common-course-pairs.sql
@@ -2,9 +2,7 @@
 -- query the students who meet the 1st condition
 WITH first_condition_qualifying_students AS (
     (SELECT
-        user_id--,
-        ---AVG(course_rating) AS "avg_rating",
-        ---COUNT(*) AS "courses_count"
+        user_id
     FROM course_completions
     GROUP BY user_id
     HAVING AVG(course_rating) >= 4 AND COUNT(*) >= 5))
@@ -20,7 +18,6 @@ all_conditions_qualifying_students AS
     FROM course_completions c1
     JOIN first_condition_qualifying_students q ON q.user_id = c1.user_id)
 
-
 SELECT
     first_course,
     second_course,
@@ -29,9 +26,6 @@ FROM all_conditions_qualifying_students
 WHERE next_completion_date IS NOT NULL
 GROUP BY first_course, second_course
 ORDER BY transition_count DESC, LOWER(first_course), LOWER(second_course)
---JOIN course_completions c2 ON c1.user_id = c2.user_id
---WHERE c.user_id IN (SELECT user_id FROM qualifying_students)
-
 
 --> identify skill mastery pathways
   --> consider only top performing students (at least 5 completed with average rating of 4 or higher)
