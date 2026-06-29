5c7f788c41d89231c4de15770ae5820ac17aadb8
klaajane <karame.laajane5@gmail.com>
2026-06-28 21:30:13 -0500

    Time: 222 ms (68.18%), Space: 0B (100%) - LeetHub

--- 2084-drop-type-1-orders-for-customers-with-type-0-orders/2084-drop-type-1-orders-for-customers-with-type-0-orders.sql
+++ 2084-drop-type-1-orders-for-customers-with-type-0-orders/2084-drop-type-1-orders-for-customers-with-type-0-orders.sql
@@ -1,36 +1,35 @@
--------------------------------------------- SOLUTION -------------------------------------------
-WITH order_type AS (
+--------------------------------------- SOLUTION -------------------------------------
+-- Find customers with at least one order type 0
+WITH customers_0_order_type AS 
+    (
     SELECT
-        ---order_id,
         customer_id,
-        ---order_type,
-        SUM(CASE 
-            WHEN order_type = 0 THEN 1
-                ELSE 0
-            END) AS "ordertype0_count"
-    FROM
-        Orders
+        SUM(CASE WHEN order_type = 0 THEN 1 ELSE 0 END) "0_order_check"
+    FROM orders
     GROUP BY 1
-),
+    HAVING SUM(CASE WHEN order_type = 0 THEN 1 ELSE 0 END) >= 1
+    )
+,
 
-orderstype0 AS (
-SELECT o.order_id, o.customer_id, o.order_type 
-FROM orders o
-WHERE o.order_type = 0 AND EXISTS (SELECT 1 FROM order_type ot WHERE 
-o.customer_id = ot.customer_id AND ordertype0_count >= 1)
-),
+records_to_exclude AS 
+    (
+    SELECT
+        order_id,
+        customer_id,
+        order_type
+    FROM orders o
+    WHERE (order_type = 1
+    AND customer_id IN (SELECT customer_id FROM customers_0_order_type))
+    )
 
-orderstype_no_0 AS(
-SELECT o.order_id, o.customer_id, o.order_type 
-FROM orders o 
-WHERE EXISTS (SELECT 1 FROM order_type ot WHERE ot.customer_id = o.customer_id AND ordertype0_count = 0)
-)
+SELECT * FROM orders
 
-SELECT * FROM orderstype0
-    UNION ALL
-SELECT * FROM orderstype_no_0
----------------------------------------------- NOTES --------------------------------------------
---> report all the orders:
-    --> customer has at least order of type 0, no type 1
-    --> otherwise, report all orders of the customer
--------------------------------------------------------------------------------------------------
+EXCEPT 
+
+SELECT * FROM records_to_exclude
+    
+---------------------------------------- NOTES ---------------------------------------
+--> query goal: report all orders based on the following criteria:
+    --> don't report any order type 1 from customer with COUNT(order(0)) >= 0
+    --> otherwise report all order
+--------------------------------------------------------------------------------------
