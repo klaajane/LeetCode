415696718dac42f41ad6f1dab25839731ce41c33
klaajane <karame.laajane5@gmail.com>
2026-06-30 22:23:38 -0500

    Time: 414 ms (26.32%), Space: 0B (100%) - LeetHub

--- 2292-products-with-three-or-more-orders-in-two-consecutive-years/2292-products-with-three-or-more-orders-in-two-consecutive-years.sql
+++ 2292-products-with-three-or-more-orders-in-two-consecutive-years/2292-products-with-three-or-more-orders-in-two-consecutive-years.sql
@@ -1,31 +1,24 @@
 --------------------------------------------- SOLUTION ------------------------------------------
-WITH products_ordered AS (
+WITH product_aggreggated AS
+    (
     SELECT
-        order_id,
         product_id,
-        EXTRACT(YEAR FROM purchase_date) AS "year"
-    FROM
-        orders
-),
-
-groups AS (
-    SELECT
-        year,
-        product_id,
-        COUNT(*) AS order_count
-    FROM
-        products_ordered
+        EXTRACT(YEAR FROM purchase_date) "year",
+        COUNT(*) "count"
+    FROM orders 
     GROUP BY 1, 2
-)
+    HAVING COUNT(*) >= 3
+    )
 
-SELECT DISTINCT 
-    g1.product_id 
-FROM groups g1
-    INNER JOIN groups g2 
-    ON g1.product_id = g2.product_id 
-    AND g1.year = g2.year + 1
-WHERE g1.order_count >= 3 
-    AND g2.order_count >= 3
+SELECT DISTINCT product_id
+FROM
+    (SELECT
+        *,
+        year - ROW_NUMBER() OVER (PARTITION BY product_id
+                                ORDER BY year) "grp_id"
+    FROM product_aggreggated)
+GROUP BY product_id, grp_id
+HAVING COUNT(*) >= 2
 ---------------------------------------------- NOTES --------------------------------------------
 --> IDs of all products that were ordered 3 or more times in 2 consecutive years
 -------------------------------------------------------------------------------------------------
