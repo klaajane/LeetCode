1d36785c2b310f694468322f1d2974b7ea1aab2a
klaajane <karame.laajane5@gmail.com>
2026-08-01 14:24:24 -0500

    Time: 6 ms (56.62%), Space: 26.3 MB (99.22%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,7 +1,7 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         if k > len(nums):
-            k = k % len(nums)
+            k %= len(nums)
 
         nums_reversed = nums[::-1]
         after_k = nums_reversed[:k][::-1]
