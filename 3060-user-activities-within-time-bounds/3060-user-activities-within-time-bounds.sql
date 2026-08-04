e49ed06fd45e648a612539c83f3edec2dc780cf3
klaajane <karame.laajane5@gmail.com>
2026-08-03 21:04:25 -0500

    Time: 202 ms (92%), Space: 0B (100%) - LeetHub

--- 3060-user-activities-within-time-bounds/3060-user-activities-within-time-bounds.sql
+++ 3060-user-activities-within-time-bounds/3060-user-activities-within-time-bounds.sql
@@ -1,13 +1,19 @@
 -------------------------------------------- SOLUTION -------------------------------------------
+WITH lagged_sessions AS (
+    SELECT 
+        user_id,
+        LAG(session_end) OVER (PARTITION BY user_id, session_type
+                            ORDER BY session_start) AS "previous_session",
+        session_start AS "current_session"    
+    FROM
+        sessions)
+
 SELECT DISTINCT
-    s1.user_id
-FROM sessions s1
-JOIN sessions s2 
-    ON s1.user_id = s2.user_id 
-    AND s1.session_type = s2.session_type
-    AND s1.session_id != s2.session_id -- to avoid including sessions with the same id more than once)
-    AND ABS(EXTRACT(epoch FROM (s2.session_end -  s1.session_start)) / 3600)  <= 12
-ORDER BY user_id
+    user_id
+FROM lagged_sessions  
+WHERE previous_session IS NOT NULL
+  AND ABS(EXTRACT(epoch FROM (current_session - previous_session)) / 3600) <= 12    
+ORDER BY user_id             
 ---------------------------------------------- NOTES --------------------------------------------
 --> extract users with at least 2 session of the same type (viewer or streamer)
 --> max gap between sessions = 12 hours
