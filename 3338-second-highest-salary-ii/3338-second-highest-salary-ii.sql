b59321767b01184c56d06a81ec2dbfb5fc6d93a8
klaajane <karame.laajane5@gmail.com>
2026-08-15 14:25:39 -0500

    Time: 199 ms (24%), Space: 0B (100%) - LeetHub

--- 3338-second-highest-salary-ii/3338-second-highest-salary-ii.sql
+++ 3338-second-highest-salary-ii/3338-second-highest-salary-ii.sql
@@ -1,15 +1,29 @@
-SELECT
-    emp_id,
-    dept
-FROM
-    (
+--------------------------------------------- SOLUTION ------------------------------------------
+--> STEP 1: apply DENSE_RANK() 
+WITH ranked_salaries AS NOT MATERIALIZED (
     SELECT
         emp_id,
-        salary,
         dept,
-        DENSE_RANK() OVER (PARTITION BY dept 
-                            ORDER BY salary DESC) AS "salary_rnk"
-    FROM employees
-    ) ranked_salaries
-WHERE salary_rnk = 2
+
+        DENSE_RANK() OVER (
+            PARTITION BY dept
+            ORDER BY salary DESC
+        ) as rnk
+
+    FROM employees)
+
+--> STEP 2: filter by rnk to display 2nd highest salary
+SELECT
+    emp_id,
+    dept 
+FROM ranked_salaries
+WHERE rnk = 2
 ORDER BY emp_id
+---------------------------------------------- NOTES ---------------------------------------------
+--> GOAL:
+    --> find employees who earnd 2nd salary in each dept
+    --> tie ==> return all employees
+
+--> ORDER BY:
+    --> emp_id ASC
+--------------------------------------------------------------------------------------------------
