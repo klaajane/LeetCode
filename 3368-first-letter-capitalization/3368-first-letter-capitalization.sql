219cf549eeb6e63dd207d7bef10385cacb368763
klaajane <karame.laajane5@gmail.com>
2026-08-15 17:43:43 -0500

    Time: 190 ms (59.52%), Space: 0B (100%) - LeetHub

--- 3368-first-letter-capitalization/3368-first-letter-capitalization.sql
+++ 3368-first-letter-capitalization/3368-first-letter-capitalization.sql
@@ -6,6 +6,6 @@
 
 SELECT
     content_id,
-    content_text AS "original_text",
-    INITCAP(content_text) AS "converted_text"
+    content_text AS original_text,
+    INITCAP(content_text) AS converted_text
 FROM user_content
