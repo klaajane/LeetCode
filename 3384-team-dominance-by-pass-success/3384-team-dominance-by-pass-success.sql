9cf7287835713045c90eed95430431dd78b6ab3c
klaajane <karame.laajane5@gmail.com>
2026-08-16 13:56:57 -0500

    Time: 221 ms (15%), Space: 0B (100%) - LeetHub

--- 3384-team-dominance-by-pass-success/3384-team-dominance-by-pass-success.sql
+++ 3384-team-dominance-by-pass-success/3384-team-dominance-by-pass-success.sql
@@ -1,9 +1,29 @@
+-- plan: 
+    --> join the passes table to the teams table
+    --> use some CASE statement logic to derive the half num
+    --> compare the teams of the players to calcualte the dominance score
+
+WITH passes_analysis AS (
+    SELECT
+        t1.team_name,
+
+        -- half_number:
+        CASE WHEN time_stamp <= '45:00' THEN 1 ELSE 2 END AS half_number, -- we have to be inclusive
+
+        -- check players' teams:
+        CASE WHEN t1.team_name = t2.team_name THEN 1 ELSE -1 END AS pre_calc_dominance_score
+
+    FROM passes p
+    INNER JOIN teams t1
+        ON t1.player_id = p.pass_from
+    INNER JOIN teams t2
+        ON t2.player_id = p.pass_to -- PAY ATTENTION TO THE ALIAS BEING USED
+)
+
 SELECT
-    t1.team_name,
-    CASE WHEN time_stamp <= '45:00' THEN 1 ELSE 2 END AS half_number,
-    SUM(CASE WHEN t1.team_name = t2.team_name THEN 1 ELSE -1 END) AS dominance
-FROM passes p
-INNER JOIN teams t1 ON t1.player_id = p.pass_from
-INNER JOIN teams t2 ON t2.player_id = p.pass_to
-GROUP BY t1.team_name, half_number
-ORDER BY t1.team_name, half_number
+    team_name,
+    half_number,
+    SUM(pre_calc_dominance_score) AS dominance
+FROM passes_analysis 
+GROUP BY team_name, half_number
+ORDER BY team_name,half_number
