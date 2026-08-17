6129e8e2d9d1fdf4c85c6cc4fd850edf0156083f
klaajane <karame.laajane5@gmail.com>
2026-08-17 18:50:43 -0500

    Time: 221 ms (27.43%), Space: 0B (100%) - LeetHub

--- 3475-dna-pattern-recognition/3475-dna-pattern-recognition.sql
+++ 3475-dna-pattern-recognition/3475-dna-pattern-recognition.sql
@@ -1,36 +1,21 @@
--------------------------------------------- SOLUTION -------------------------------------------
+-- Mutiple CASE statements to check the conditions for the patterns:
+
 SELECT
     sample_id,
     dna_sequence,
     species,
-    --- ATG Condition:
-    CASE
-        WHEN dna_sequence LIKE 'ATG%' THEN 1 ELSE 0
-    END AS "has_start",
-    --- TAA, TAG, TGA Condition:
-    CASE
-        WHEN RIGHT(dna_sequence, 3) IN ('TAA','TAG','TGA') THEN 1 ELSE 0
-    END AS "has_stop",
-    --- ATAT Condition:
-    CASE
-        WHEN dna_sequence LIKE '%ATAT%' THEN 1 ELSE 0 -- helped optimize the query
-        --WHEN dna_sequence ~ 'ATAT' THEN 1 ELSE 0
-    END AS "has_atat",
-    --- GGG Condition:
-    CASE
-        WHEN dna_sequence LIKE '%GGG%' THEN 1 ELSE 0 -- helped optimize the query
-        --WHEN dna_sequence ~ 'G{3,}' THEN 1 ELSE 0
-    END AS "has_ggg"
-FROM
-    samples
-ORDER BY
-    sample_id
----------------------------------------------- NOTES --------------------------------------------
---> Sequences that start with ATG (a common start codon)
---> Sequences that end with either TAA, TAG, or TGA (stop codons)
---> Sequences containing the motif ATAT (a simple repeated pattern)
---> Sequences that have at least 3 consecutive G (like GGG or GGGG)
------------------------------------------- OPTIMZATINON -----------------------------------------
---> the above query was slow first time I ran it, here's some a tip I found to optimize it:
---> Regex '~' is performance taxing. Use LIKE instead
--------------------------------------------------------------------------------------------------
+
+-- condition 1: start with ATG:
+    CASE WHEN LEFT(dna_sequence, 3) = 'ATG' THEN 1 ELSE 0 END AS has_start,
+
+-- coniditon 2: end with TAA, TAG, or TGA:
+    CASE WHEN RIGHT(dna_sequence, 3) IN ('TAA', 'TAG', 'TGA') THEN 1 ELSE 0 END AS has_stop,
+
+-- condition 3: contains ATAT 
+    CASE WHEN  dna_sequence LIKE '%ATAT%'THEN 1 ELSE 0 END AS has_atat,
+
+-- condition 4: at least consecutive 3 G's 
+    CASE WHEN dna_sequence ~ 'G{3,}' THEN 1 ELSE 0 END AS has_ggg
+
+FROM samples
+ORDER BY sample_id ASC
