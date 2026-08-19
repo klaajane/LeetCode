4b59afa143c987c511bdb5e2541a0a9e52a1c6db
klaajane <karame.laajane5@gmail.com>
2026-08-18 20:38:08 -0500

    Time: 217 ms (63.72%), Space: 0B (100%) - LeetHub

--- 3564-seasonal-sales-analysis/3564-seasonal-sales-analysis.sql
+++ 3564-seasonal-sales-analysis/3564-seasonal-sales-analysis.sql
@@ -1,55 +1,53 @@
--------------------------------------------- SOLUTION -------------------------------------------
-WITH product_sales_merged AS (SELECT
-                                --s.product_id,
-                                s.sale_date,
-                                CASE
-                                    WHEN EXTRACT(MONTH FROM s.sale_date) BETWEEN 3 AND 5 THEN 'Spring'
-                                    WHEN EXTRACT(MONTH FROM s.sale_date) BETWEEN 6 AND 8  THEN 'Summer'
-                                    WHEN EXTRACT(MONTH FROM s.sale_date) BETWEEN 9 AND 11 THEN 'Fall'
-                                    ELSE 'Winter'
-                                END AS "season",
-                                p.category,
-                                s.quantity,
-                                s.price
-                                --p.*
-                             FROM
-                                sales s
-                             INNER JOIN
-                                products p
-                                ON
-                                p.product_id = s.product_id),
-main_product_sales_table AS (SELECT
-                                season,
-                                category,
-                                SUM(quantity) AS total_quantity,
-                                SUM(quantity * price) AS total_revenue
-                            FROM 
-                                product_sales_merged
-                            GROUP BY
-                                1,2),
-ordered_product_sales AS (SELECT
-                                *,
-                                DENSE_RANK() OVER (PARTITION BY season 
-                                                   ORDER BY total_quantity DESC,
-                                                            total_revenue DESC) AS rnk
-                            FROM
-                                main_product_sales_table)
+WITH sales_by_month_product AS (
+    SELECT 
+        product_id,
+        EXTRACT(MONTH FROM sale_date) AS month,
+        quantity,
+        quantity * price AS revenue
+    FROM sales
+    )
+,
+
+seasonal_sales AS (
+    SELECT
+    product_id,
+    CASE
+        WHEN month IN (9, 10, 11) THEN 'Fall'
+        WHEN month IN (12, 1, 2) THEN 'Winter'
+        WHEN month IN (3, 4, 5) THEN 'Spring'
+        WHEN month IN (6, 7, 8) THEN 'Summer'
+    END AS season,
+    quantity,
+    revenue
+FROM sales_by_month_product 
+)
+,
+
+master_final AS (
+    SELECT
+        s.season,
+        p.category,
+        SUM(quantity) AS total_quantity,
+        SUM(revenue) AS total_revenue,
+        DENSE_RANK() OVER (
+            PARTITION BY season
+            ORDER BY 
+                SUM(quantity) DESC,
+                SUM(revenue) DESC
+        ) AS rnk
+    FROM seasonal_sales s
+    JOIN products p
+        ON p.product_id = s.product_id
+    GROUP BY s.season, p.category
+    ORDER BY s.season
+    )
+
 SELECT
     season,
     category,
     total_quantity,
     total_revenue
-FROM
-    ordered_product_sales
-WHERE
-    rnk = 1
----------------------------------------------- NOTES --------------------------------------------
---> find most popular product category for each season
---> Winter: December, January, February
---> Spring: March, April, May
---> Summer: June, July, August
---> Fall: September, October, November
---> Popularity: the total quantity sold in that season
---> tie => select category with highest total revenue (quanity * price)
---> order by season ASC
--------------------------------------------------------------------------------------------------
+FROM master_final
+WHERE rnk = 1
+ORDER BY season
+-- don't use window function with group by because the window function 
