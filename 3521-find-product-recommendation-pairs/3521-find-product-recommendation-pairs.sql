6916e0f3818f133c0410b6c45e6025ca1364492e
klaajane <karame.laajane5@gmail.com>
2026-08-18 19:41:10 -0500

    Time: 306 ms (79.12%), Space: 0B (100%) - LeetHub

--- 3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
+++ 3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
@@ -1,51 +1,37 @@
--------------------------------------------- SOLUTION -------------------------------------------
-WITH paired_products AS (SELECT
-                            p1.product_id AS product1_id,
-                            p2.product_id AS product2_id,
-                            p1.quantity AS product1_quantity,
-                            p2.quantity AS product2_quantity
-                        FROM
-                            ProductPurchases p1
-                        INNER JOIN
-                            ProductPurchases p2
-                            ON
-                            p1.user_id = p2.user_id
-                        WHERE
-                            p1.product_id < p2.product_id),
-paired_products_count AS (SELECT
-                            p.product1_id,
-                            p.product2_id,
-                            i1.category AS product1_category,
-                            i2.category AS product2_category,
-                            COUNT(*) AS customer_count
-                        FROM
-                            paired_products p
-                        INNER JOIN
-                            ProductInfo i1
-                            ON 
-                            p.product1_id = i1.product_id
-                        INNER JOIN
-                            ProductInfo i2
-                            ON 
-                            p.product2_id = i2.product_id
-                        GROUP BY
-                            product1_id,
-                            product2_id,
-                            i1.category,
-                            i2.category)
-SELECT 
-    * 
-FROM 
-    paired_products_count
-WHERE
-    customer_count >= 3
+-- Write your PostgreSQL query statement below
+
+-- self-join : to get the different pairs for each user
+
+WITH pairs_by_users AS (
+    SELECT
+        p1.user_id,
+        p1.product_id AS product1_id,
+        p2.product_id AS product2_id
+    FROM productpurchases p1
+    JOIN productpurchases p2
+        ON p1.user_id = p2.user_id
+        AND p1.product_id < p2.product_id
+)
+
+-- self join: using the pairs to find out the couts
+SELECT
+    p.product1_id,
+    p.product2_id,
+    pi1.category AS product1_category,
+    pi2.category AS product2_category,
+    COUNT(DISTINCT p.user_id) AS customer_count
+FROM pairs_by_users p
+JOIN productinfo pi1 ON p.product1_id = pi1.product_id
+JOIN productinfo pi2 ON p.product2_id = pi2.product_id
+GROUP BY
+    p.product1_id, p.product2_id,
+    pi1.category, pi2.category
+HAVING COUNT(DISTINCT p.user_id) >= 3
 ORDER BY
     customer_count DESC,
     product1_id ASC,
     product2_id ASC
----------------------------------------------- NOTES --------------------------------------------
---> identify DISTINCT product pairs frequently purchased (product1_id < product2_id)
---> determine the # of customers who purchased both products
---> product as a recommendation: if at least 3 different customers have bought it
---> order by customer_count in DESC, tie => product1_id ASC, product2_id ASC
--------------------------------------------------------------------------------------------------
+-- NOTES: 
+
+-- I wouldnt bring in the categories from the category table until I find the qualifying records (count >= 3)
+-- use '<' instead of '<>' to avoid duplication
