3be5619a948e3da766d5964df095e69bf6c38ecf
klaajane <karame.laajane5@gmail.com>
2026-08-22 11:42:18 -0500

    Time: 291 ms (81.07%), Space: 0B (100%) - LeetHub

--- 3642-find-books-with-polarized-opinions/3642-find-books-with-polarized-opinions.sql
+++ 3642-find-books-with-polarized-opinions/3642-find-books-with-polarized-opinions.sql
@@ -1,42 +1,55 @@
--------------------------------------------- SOLUTION -------------------------------------------
-WITH polarized_opinion AS (SELECT
-                                book_id,
-                                COUNT(*) AS "reading_session_count",
-                                MAX(session_rating) - MIN(session_rating) AS "rating_spread",
-                                COUNT(*) FILTER (WHERE session_rating >= 4 
-                                                    OR session_rating <= 2) AS "extreme_count",
-                                COUNT(*) FILTER (WHERE session_rating >= 4) AS "highest_extreme",
-                                COUNT(*) FILTER (WHERE session_rating <= 2) AS "lowest_extreme"
-                            FROM    
-                                reading_sessions
-                            GROUP BY
-                                1)
-SELECT
-    p.book_id,
-    b.title,
-    b.author,
-    b.genre,
-    b.pages,
-    p.rating_spread,
-    ROUND(1.0 * p.extreme_count / p.reading_session_count, 2) AS "polarization_score"
-FROM
-    polarized_opinion p
-INNER JOIN
-    books b ON b.book_id = p.book_id
-WHERE
-    p.reading_session_count >= 5
-    AND
-    1.0 * p.extreme_count / p.reading_session_count >= 0.6
-    AND
-    (p.highest_extreme != 0 AND p.lowest_extreme != 0)
-ORDER BY
-    polarization_score DESC,
-    b.title DESC
----------------------------------------------- NOTES --------------------------------------------
---> polarized opinions: books that receive both very high/low ratings from different readers
---> only include books that have at least 5 reading sessions
---> calculate rating spread (highest_rating - lowest_rating)
---> polarization score: # of extreme ratings / total sessions
---> only include books with a polarization score >= 0.6
---> order by polarization socer DESC, title DESC
--------------------------------------------------------------------------------------------------
+-- Write your PostgreSQL query statement below
+
+-- CASE STATEMENT for flagging books with polarized opinions
+
+-- SUB QUERY to return books that have at least 5 reading books
+
+-- 
+WITH reading_sessions_polarized_check AS (
+    SELECT
+        book_id,
+        COUNT(*) AS total_session,
+        MIN(session_rating) AS minrating,
+        MAX(session_rating) AS maxrating,
+        sum(case when session_rating>3 then 1 else 0 end)highrating,
+        sum(case when session_rating<3 then 1 else 0 end )lowrating,
+        sum(case when session_rating>3 or session_rating<3 then 1 else 0 end) extremes
+    FROM reading_sessions
+    WHERE book_id IN (
+        SELECT book_id
+        FROM reading_sessions
+        GROUP BY book_id
+        HAVING COUNT(*) >= 5
+    )
+    GROUP BY book_id
+)
+
+,
+polarization_score_by_book AS (
+    SELECT 
+        r.book_id,
+        b.title,
+        b.author,
+        b.genre,
+        b.pages,
+        maxrating - minrating AS rating_spread,
+        ROUND(
+            extremes * 1.0
+            /
+            total_session
+            ,2
+        ) AS polarization_score
+    FROM reading_sessions_polarized_check r
+    JOIN books b
+        ON b.book_id = r.book_id
+    WHERE
+        total_session > 4
+        AND
+        highrating > 0
+        AND
+        lowrating > 0
+    ORDER BY polarization_score DESC, title DESC
+    )
+
+SELECT * FROM polarization_score_by_book
+WHERE polarization_score >= 0.6
