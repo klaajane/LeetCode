0a4a8cc85f5fd1c5ac7ef4e9235d773c597e61d1
klaajane <karame.laajane5@gmail.com>
2026-09-04 19:41:44 -0500

    Time: 191 ms (55.87%), Space: 0B (100%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -13,25 +13,28 @@
 
 --> WHERE only show records after the 6th
 
+--> RANGE is appropriate here because the requirement is based on a date interval ratehre than the number of rows
+--> ROWS means six preceding rows, not six preceding calendar days
+
 WITH customers_metrics AS (
     SELECT
-        visited_on,
-        SUM(amount) OVER (
-            ORDER BY visited_on
-            RANGE BETWEEN INTERVAL '6 DAYS' PRECEDING
-            AND CURRENT ROW
-        ) AS amount,
-        ROUND(
-            AVG(amount) OVER (
-            ORDER BY visited_on
-            RANGE BETWEEN INTERVAL '6 DAYS' PRECEDING
-            AND CURRENT ROW
-            )
-            , 2) AS average_amount
+    visited_on,
+
+    ROUND(SUM(amount) OVER w, 2) AS amount,
+
+    ROUND(AVG(amount) OVER w, 2) AS average_amount
+    
     FROM (
         SELECT visited_on, SUM(amount) AS amount
         FROM customer
         GROUP BY visited_on
+    )
+    
+    WINDOW w AS (
+    ORDER BY visited_on
+    RANGE BETWEEN INTERVAL '6 DAYS' PRECEDING
+              AND CURRENT ROW
+
     )
 )
 
@@ -44,7 +47,13 @@ WHERE visited_on - (SELECT MIN(visited_on) FROM customer) >= 6
 ORDER BY 
     visited_on ASC
     
+-- if this was a real porduction table with a lot of data:
+    --> Do we have an index on visited_on?
 
+-- CREATE INDEX indx_customer_visited_on
+-- on customer(visited_on)
+
+-- can make the query readable using WINDOW, since we have the same window logic repeated in the query
 
 
 
