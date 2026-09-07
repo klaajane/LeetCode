fec74a73616183781b2e665a8db08ebbcc0252b3
klaajane <karame.laajane5@gmail.com>
2026-09-07 14:18:05 -0500

    Time: 302 ms (37.04%), Space: 0B (100%) - LeetHub

--- 1459-rectangles-area/1459-rectangles-area.sql
+++ 1459-rectangles-area/1459-rectangles-area.sql
@@ -1,35 +1,33 @@
 --------------------------------------------- SOLUTION ------------------------------------------
 WITH points_combined AS (
     SELECT
-        p1.id AS "id1",
-        p2.id AS "id2",
-        p1.x_value AS "x1_value",
-        p2.x_value AS "x2_value",
-        p1.y_value AS "y1_value",
-        p2.y_value AS "y2_value"
-    FROM
-        points p1
-    CROSS JOIN
-        points p2),
+        p1.id AS p1,
+        p1.x_value AS x1_value,
+        p2.x_value AS x2_value,
+        p2.id AS p2,
+        p1.y_value AS y1_value,
+        p2.y_value AS y2_value
+    FROM points p1
+    CROSS JOIN points p2
+    WHERE p1.id < p2.id 
+)
+,
 
-areas AS (
+rectangle_area AS (
     SELECT
-        *,
-        abs(x1_value - x2_value) * abs(y1_value - y2_value) AS area
-    FROM
-        points_combined)
+        p1,
+        p2,
+        ABS(x2_value - x1_value) * ABS(y2_value - y1_value) AS area
+    FROM points_combined
+)
 
-SELECT DISTINCT
-    id1 "P1",
-    id2 "P2",
-    area "AREA"
-FROM 
-    areas
-WHERE
-    area != 0
-    AND id1 < id2
-ORDER BY
-    area DESC, id1, id2
+SELECT
+    p1 AS P1,
+    p2 AS P2,
+    area AS AREA
+FROM rectangle_area
+WHERE area != 0
+ORDER BY area DESC, p1, p2
 ---------------------------------------------- NOTES --------------------------------------------
 --> report all possible axis-aligned rectangles with a non-zero area 
 -------------------------------------------------------------------------------------------------
