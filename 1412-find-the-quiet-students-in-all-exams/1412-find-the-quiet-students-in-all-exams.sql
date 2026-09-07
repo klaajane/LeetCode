16a8e20fdba4203a6fe4af82ef226f4ec7decd4c
klaajane <karame.laajane5@gmail.com>
2026-09-07 13:52:13 -0500

    Time: 217 ms (56.52%), Space: 0B (100%) - LeetHub

--- 1412-find-the-quiet-students-in-all-exams/1412-find-the-quiet-students-in-all-exams.sql
+++ 1412-find-the-quiet-students-in-all-exams/1412-find-the-quiet-students-in-all-exams.sql
@@ -1,34 +1,50 @@
---------------------------------------------- SOLUTION ------------------------------------------
-WITH highest_lowest_scores AS (
-    SELECT
-        exam_id,
-        student_id,
-        score,
-        CASE
-            WHEN MAX(score) OVER (PARTITION BY exam_id) = score THEN 1
-            WHEN MIN(score) OVER (PARTITION BY exam_id) = score THEN 1
-            ELSE 0
-        END AS "is_highest_lowest_flag"
-    FROM
-        exam)
+--> GOAL:
+    --> report students who took at least one exam but didn't score the highest or lowest score
 
-SELECT 
-    h.student_id,
-    s.student_name
-    ---SUM(h.is_highest_lowest_flag)
-FROM
-    highest_lowest_scores h
-INNER JOIN
-    student s
-    ON s.student_id = h.student_id
-GROUP BY
-    h.student_id, s.student_name
-HAVING 
-    SUM(h.is_highest_lowest_flag) = 0
-ORDER BY
-    h.student_id
----------------------------------------------- NOTES --------------------------------------------
---> quiet student: >= 1 exam, didn't score the highest or the lowest score
---> report: student_id, student_name
---> order by student_id
--------------------------------------------------------------------------------------------------
+--> clarifying questions:
+    --> am I correct to assume that retakes are not included in the Exam table?
+    --> let's say there's only one student who took an exam,
+    --> how should we handle this case? should we return the student regardless?\
+    --> are we returning the student who have been a quiet student in their exam?
+    --> or has never been a quiet student? for example, if 3 wasthe quiet student
+    --> in his exam,
+
+--> Pseudocode:
+    -- 1./ find out what the MAX and MIN for each EXAM (MAX()/MIN() OVER ())
+    WITH max_min_scores_by_exam AS (
+        SELECT
+            student_id,
+            score,
+            MAX(score) OVER (
+                PARTITION BY exam_id
+            ) AS max_score,
+            MIN(score) OVER (
+                PARTITION BY exam_id
+            ) AS min_score
+        FROM exam 
+    )
+    ,
+    -- 2./ Compare scores to the MAX and MIN (CASE)
+
+    quiet_student_check AS (
+        SELECT  
+            student_id,
+            SUM(CASE
+                    WHEN score != max_score AND score != min_score 
+                    THEN 0
+                    ELSE 1
+                END) AS is_quiet
+        FROM max_min_scores_by_exam
+        GROUP BY student_id
+    )
+
+    -- 3./ Return students who didn't score the MAX or MIN:
+
+    SELECT 
+        q.student_id,
+        s.student_name
+    FROM quiet_student_check q
+    INNER JOIN student s
+        ON s.student_id = q.student_id
+    WHERE is_quiet = 0
+    ORDER BY q.student_id
