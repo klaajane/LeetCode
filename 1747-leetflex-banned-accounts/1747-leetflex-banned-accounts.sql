6b4158ac95fdc324e156a57a3f08e50c413b1e12
klaajane <karame.laajane5@gmail.com>
2026-09-09 20:12:54 -0500

    Time: 272 ms (96.49%), Space: 0B (100%) - LeetHub

--- 1747-leetflex-banned-accounts/1747-leetflex-banned-accounts.sql
+++ 1747-leetflex-banned-accounts/1747-leetflex-banned-accounts.sql
@@ -3,12 +3,12 @@ SELECT DISTINCT
     a.account_id
 FROM
     LogInfo a
-CROSS JOIN
+INNER JOIN
     LogInfo b
-WHERE
-    a.account_id = b.account_id
+    ON a.account_id = b.account_id
     AND a.ip_address != b.ip_address
-    AND a.login <= b.logout
+WHERE
+    a.login <= b.logout
     AND a.logout >= b.login
 ---------------------------------------------- NOTES --------------------------------------------
 --> find accounts that shoulda been banned (logged in from two different IP addresses)
